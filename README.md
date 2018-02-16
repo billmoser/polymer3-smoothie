4a8912fe1799391514d9b7d1edf519d562526b7c
William Moser <wm.r.moser@gmail.com>
2018-02-16 10:44:43 -0500

    updated documentation

--- README.md
+++ README.md
@@ -18,12 +18,16 @@
         cd polymer3-smoothie
         yarn install
         
-5.  To preview your element, run the Polymer development server from the repo directory:
+5. At the moment, this element is built on Polymer 3.0 preview, which still has a few issues.
+   tests are failing due to an issue with lodash, and in order to use the element, you'll need to rename a directory in node_modules after the install:
+
+        mv node_modules/prismjs node_modules/prism
+
+6.  To preview your element, run the Polymer development server from the repo directory:
 
         polymer serve --npm
         
     Open `localhost:8081` in your browser.
 
 
-
-Note - had to move node_modules/prismjs to node_modules/prism -- this is a known issue, see https://github.com/PolymerElements/marked-element/issues/81
+<!--- Note - had to move node_modules/prismjs to node_modules/prism -- this is a known issue, see https://github.com/PolymerElements/marked-element/issues/81 --->
--- polymer-smoothie.js
+++ polymer-smoothie.js
@@ -4,6 +4,21 @@ import { template } from './polymer-smoothie_template.js';
 /**
  *  Wrapper for SmoothieCharts real-time graphing library (http://smoothiecharts.org)
  *
+ *  #### Example
+ *       <polymer-smoothie id="chart" 
+ *           on-connected="_onConnected"
+ *           delay="33"
+ *           canvas='{"width": 560, "height": 200}'
+ *           config='{
+ *               "maxValue": 100,
+ *               "minValue": 0,
+ *               "grid": { "strokeStyle": "rgb(125, 0, 0)", "fillStyle": "rgb(60, 0, 0)",
+ *               "lineWidth": 1, "millisPerLine": 250, "verticalSections": 6 },
+ *               "labels": { "fillStyle": "rgb(255, 255, 255)" }}'>
+ *       </polymer-smoothie>
+ *
+ *  @customElement
+ *  @polymer
  *  @demo demo/index.html
  */
 class PolymerSmoothie extends Element {
@@ -12,12 +27,24 @@ class PolymerSmoothie extends Element {
 
     static get properties() {
         return {
+            /** The canvas containing the chart. */
             canvas: Object,
+            /** 
+             * The smoothie chart.  This can be directly manipulated using the
+             * api described at http://smoothiecharts.org
+             */
             chart: Object,
+            /**
+             * The chart's configuration, a JSON string
+             */
             config: {
                 type: Object,
                 value: function() { return {}; }
             },
+            /**
+             * The charts delay.  For smoothing purposes -- see the original
+             * api docs for more information.
+             */
             delay: {
                 type: Number,
                 value: 100
@@ -25,6 +52,12 @@ class PolymerSmoothie extends Element {
         }
     }
 
+    /**
+     * Create the chart amnd dispatch `connected` event with
+     * `detail: {id: this.id}` to indicate the event source
+     *
+     * @event connected
+     */
     connectedCallback() {
         super.connectedCallback();
         this.chart = new SmoothieChart(this.config);
@@ -37,12 +70,24 @@ class PolymerSmoothie extends Element {
             }));
     }
 
+    /**
+     * Create a new time series object, and add it to the chart.
+     * 
+     * @param {String} JSON style info for the result.
+     *
+     * @return {Object} The new `TimeSeries` object.
+     */
     newTimeSeries(style) {
         var result = new TimeSeries();
         this.chart.addTimeSeries(result, style);
         return result;
     }
 
+    /**
+     * Specify configuratuion options for the chart.
+     *
+     * @param {String} JSON config for the chart.
+     */
     chartOptions(options) {
         for (let key in options) {
             this.chart.options[key] = options[key];
